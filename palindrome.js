b765e92728b81de5291bd772fee4c8829d0ab652
Asma Charef <asma.charef@hotmail.com>
2021-06-13 13:07:58 -0500

    Updated the examples

--- palindrome.js
+++ palindrome.js
@@ -1,9 +1,6 @@
-function reverse(str) {
-    return str.split('').reverse().join('');
- }
- 
- function isPalindrome(str) {
-    return reverse(str) === str;
+function isPalindrome(str) {
+    let reverseStr = str.split('').reverse().join('');
+    return reverseStr === str;
  }
  
  module.exports = isPalindrome;
--- spec/palindrome.spec.js
+++ spec/palindrome.spec.js
@@ -5,22 +5,16 @@ describe("Palindrome tests", function () {
     it("recognizes a non-palindrome", function () {
         expect(isPalindrome("Hello")).toBe(false);
     });
+    it("should be case-sensitive", function(){
+        assert.strictEqual(isPalindrome("Hello"), false);
+    });
     it("knows that an empty string is a palindrome", function () {
         expect(isPalindrome("")).toBe(true);
     });
-    it("knows that an entirely non-alphanumeric string is not a palindrome", function () {
-        expect(isPalindrome("!@# #@!")).toBe(true);
-    });
-    it("should return true for a single letter", function(){
-        assert.strictEqual(isPalindrome("a"), true);
-    });
-    it("should return true for a single letter repeated", function(){
+    it("should return true for a same letter repeated", function(){
         assert.strictEqual(isPalindrome("aaa"), true);
     });
-    it("should be case-sensitive", function(){
-        assert.strictEqual(isPalindrome("abA"), false);
-    });
     it("should consider whitespace", function(){
-        assert.strictEqual(isPalindrome("so many dynamos"), false);
+        assert.strictEqual(isPalindrome("Top spot"), false);
     });
 });
